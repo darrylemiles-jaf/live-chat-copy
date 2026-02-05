2aa997843e8bc1921f2b8a341a69d49147b633f8
darrylemiles-jaf <darryle.jafdigitalgroupinc@gmail.com>
2026-02-05 17:14:08 +0800

    feat: initialize live chat API with Express and necessary configurations

---
+++ server/server.js
@@ -0,0 +1,47 @@
+import {
+  colours,
+  PROJECT_NAME
+} from './constants/constants.js';
+
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT
+const API_VERSION = process.env.API_VERSION
+
+const corsOptions = {
+  origin: '*',
+  credentials: true,
+  optionSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
+app.use(express.json({ limit: '100mb' }));
+app.use(express.urlencoded({ limit: '100mb', extended: true, parameterLimit: 50000 }));
+
+app.get(`/api/${API_VERSION}`, (req, res) => {
+  res.send('API Gateway is running...');
+});
+
+const startServer = async () => {
+  try {
+    app.listen(PORT, () =>
+      console.log(
+        colours.fg.yellow,
+        `${PROJECT_NAME} API is running in ${process.env.NODE_ENV} mode on port ${PORT}`,
+        colours.reset
+      )
+    );
+
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+};
+
+// Invoke Start Application Function
+startServer();
