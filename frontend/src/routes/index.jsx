a5e28aeb87712b824dcfc1f828f415524f655db5
darrylemiles-jaf <darryle.jafdigitalgroupinc@gmail.com>
2026-02-05 14:35:43 +0800

    feat: add NotFound page and integrate into routing

--- frontend/src/routes/index.jsx
+++ frontend/src/routes/index.jsx
@@ -3,9 +3,19 @@ import { createBrowserRouter } from 'react-router-dom';
 // project imports
 import MainRoutes from './MainRoutes';
 import LoginRoutes from './LoginRoutes';
+import NotFound from '../pages/not-found';
 
 // ==============================|| ROUTING RENDER ||============================== //
 
-const router = createBrowserRouter([MainRoutes, LoginRoutes], { basename: import.meta.env.VITE_APP_BASE_NAME });
+const routes = [
+  {
+    path: '*',
+    element: <NotFound />,
+  },
+  MainRoutes,
+  LoginRoutes
+];
+
+const router = createBrowserRouter(routes, { basename: import.meta.env.VITE_APP_BASE_NAME });
 
 export default router;
