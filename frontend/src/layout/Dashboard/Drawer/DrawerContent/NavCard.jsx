166be7c9b48dd682726f148e1e1e42332186d674
zedrick <Zedrick.jafdigitalgroupinc@gmail.com>
2026-02-06 15:20:09 +0800

    feat: update navigation links

--- frontend/src/layout/Dashboard/Drawer/DrawerContent/NavCard.jsx
+++ frontend/src/layout/Dashboard/Drawer/DrawerContent/NavCard.jsx
@@ -21,14 +21,11 @@ export default function NavCard() {
       <Stack alignItems="center" spacing={2.5}>
         <CardMedia component="img" image={avatar} sx={{ width: 112 }} />
         <Stack alignItems="center">
-          <Typography variant="h5">Timora</Typography>
-          <Typography variant="h6" color="secondary">
-            Go to Timora
-          </Typography>
+          <Typography variant="h5">Need Help?</Typography>
         </Stack>
         <AnimateButton>
-          <Button component={Link} target="_blank" href="https://timora.ph/" variant="contained" color="success" size="small">
-            <LogoutOutlined />
+          <Button component={Link} target="_blank" href="http://localhost:3000/portal/dashboards" variant="contained" size="small" sx={{ bgcolor: '#008E86', '&:hover': { bgcolor: '#007570' } }}>
+            Contact Developer
           </Button>
         </AnimateButton>
       </Stack>
