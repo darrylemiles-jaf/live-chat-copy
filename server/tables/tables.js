6e9fbf7e3015c3b441a28b21c1957167f4f84896
darrylemiles-jaf <darryle.jafdigitalgroupinc@gmail.com>
2026-02-09 15:21:25 +0800

    feat: add chats and messages tables to database schema

--- server/tables/tables.js
+++ server/tables/tables.js
@@ -1,9 +1,18 @@
-// users
 import usersTable from './usersTable.js';
+import chatsTable from './chatsTable.js';
+import messagesTaable from './messagesTaable.js';
+
+
 const tables = async (dbConnection) => {
   const queries = [
     // users
     usersTable,
+
+    // chats
+    chatsTable,
+
+    // messages
+    messagesTaable,
   ];
 
   for (const query of queries) {
