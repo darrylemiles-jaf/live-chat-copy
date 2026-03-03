9164e87102ba9c184f2d4f9fd965792dabb46619
darrylemiles-jaf <darryle.jafdigitalgroupinc@gmail.com>
2026-03-03 15:04:19 +0800

    fix: remove allowedRoles middleware from login route

--- server/routes/usersRoutes.js
+++ server/routes/usersRoutes.js
@@ -1,10 +1,10 @@
 import { getUsers, getSingleUser, authUser, updateUserStatus } from '../controllers/usersControllers.js'
 import express from 'express'
-import { allowedRoles, protect } from '../middlewares/authMiddleware.js'
+import { protect } from '../middlewares/authMiddleware.js'
 
 const router = express.Router()
 
-router.post('/login', allowedRoles, authUser)
+router.post('/login', authUser)
 router.get('/', protect, getUsers)
 router.get('/:id', protect, getSingleUser)
 router.patch('/:id/status', protect, updateUserStatus)
