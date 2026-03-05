668c55d5691d5344aed1510e3dae535153f72b51
darrylemiles-jaf <darryle.jafdigitalgroupinc@gmail.com>
2026-03-05 15:42:37 +0800

    fix: import resolve from path and update rollup input configuration

--- frontend/vite.config.mjs
+++ frontend/vite.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import jsconfigPaths from 'vite-jsconfig-paths';
 import path from 'path';
+import { resolve } from 'path'
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
@@ -34,6 +35,11 @@ export default defineConfig(({ mode }) => {
       sourcemap: true,
       cssCodeSplit: true,
       rollupOptions: {
+        input: {
+          main: resolve(__dirname, 'index.html'),
+          login: resolve(__dirname, 'loginFake.html'),
+          dashboard: resolve(__dirname, 'fake-dashboard.html')
+        },
         output: {
           chunkFileNames: 'js/[name]-[hash].js',
           entryFileNames: 'js/[name]-[hash].js',
